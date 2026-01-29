dc1e924d5dfad35276f0fd592ee00344f644604e
phawinee092003-byte <phawinee.09.2003@gmail.com>
2026-01-29 21:23:22 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # เราจะส่งหน้า index.html ไปให้เบราว์เซอร์
+    return render_template('index.html')
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=5000)
